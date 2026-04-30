7e602a00b2050df5b12e3c9b1876811441f7cbb3
Lucas <lucaslpmoura@gmail.com>
2026-04-30 09:31:28 -0300

    Added implementation for UserRoomMediator.kt

--- src/main/kotlin/com/lucaslpmoura/kotlin_chat/server/UserRoomMediator.kt
+++ src/main/kotlin/com/lucaslpmoura/kotlin_chat/server/UserRoomMediator.kt
@@ -3,28 +3,73 @@ package com.lucaslpmoura.kotlin_chat.com.lucaslpmoura.kotlin_chat.server
 import kotlin.to
 
 class UserRoomMediator : UserRoomMediatorInteface {
-    override val userRoomMap: MutableMap<KotlinChatUser, KotlinChatRoom?> = mutableMapOf<KotlinChatUser, KotlinChatRoom?>()
-    override val roomUserMap: MutableMap<KotlinChatRoom, MutableSet<KotlinChatUser>> = mutableMapOf<KotlinChatRoom, MutableSet<KotlinChatUser>>()
+    private val userRoomMap: MutableMap<KotlinChatUser, KotlinChatRoom?> = mutableMapOf<KotlinChatUser, KotlinChatRoom?>()
+    private val roomUserMap: MutableMap<KotlinChatRoom, MutableSet<KotlinChatUser>> = mutableMapOf<KotlinChatRoom, MutableSet<KotlinChatUser>>()
 
-    override fun addUserToRoom(
-        user: KotlinChatUser,
-        room: KotlinChatRoom
-    ) {
-        if(userRoomMap[user] != null) {
-            throw Exception("User ${user.id} already in room ${room.id}")
+
+    override fun addRoom(room: KotlinChatRoom) {
+        if (roomUserMap.containsKey(room)){
+            throw Exception("Room already exists.")
         }
-        if(room.numOfUsers >= room.MAX_USERS){
-            throw Exception("Maximum number of users reached for room ${room.id}")
-        }
-        userRoomMap[user] = room
-        roomUserMap[room]!!.add(user)
+        roomUserMap[room] = mutableSetOf<KotlinChatUser>()
     }
 
-    override fun removeUserFromRoom(
-        user: KotlinChatUser,
-        room: KotlinChatRoom
-    ) {
-        TODO("Not yet implemented")
+    override fun removeRoom(room: KotlinChatRoom) {
+        if(!roomUserMap.containsKey(room)){
+            throw Exception("Room does not exist.")
+        }
+        roomUserMap.remove(room)
+    }
+
+    override fun addUser(user: KotlinChatUser) {
+        if(userRoomMap.containsKey(user)){
+            throw Exception("User already on server.")
+        }
+        userRoomMap[user] = null
+    }
+
+    override fun removeUser(user: KotlinChatUser) {
+        if(!userRoomMap.containsKey(user)){
+            throw Exception("User is not on server.")
+        }
+        userRoomMap.remove(user)
+    }
+
+
+    override fun addUserToRoom(user: KotlinChatUser, room: KotlinChatRoom) {
+        checkIfUserAndRoomExists(user, room)
+        if(room.numOfUsers >= room.MAX_USERS){
+            throw Exception("Maximum number of users reached for room ${room.id}.")
+        }
+        if(userRoomMap[user] != null) {
+            throw Exception("User ${user.id} already in room ${room.id}.")
+        }
+
+        userRoomMap[user] = room
+        roomUserMap[room]!!.add(user)
+        room.numOfUsers++
+    }
+    override fun removeUserFromRoom(user: KotlinChatUser, room: KotlinChatRoom) {
+        checkIfUserAndRoomExists(user, room)
+        if(room.numOfUsers <= 0){
+            throw Exception("Room ${room.id} already has no users.")
+        }
+        if(userRoomMap[user] != room) {
+            throw Exception("User ${user.id} is not at room ${room.id}.")
+        }
+
+        userRoomMap[user] = null
+        roomUserMap[room]!!.remove(user)
+        room.numOfUsers--
+    }
+
+    private fun checkIfUserAndRoomExists(user: KotlinChatUser, room: KotlinChatRoom) {
+        if(!roomUserMap.containsKey(room)){
+            throw Exception("Room ${room.id} does not exist.")
+        }
+        if(!userRoomMap.containsKey(user)){
+            throw Exception("User ${user.id} is not on server.")
+        }
     }
 }
 
--- src/main/kotlin/com/lucaslpmoura/kotlin_chat/server/UserRoomMediatorInteface.kt
+++ src/main/kotlin/com/lucaslpmoura/kotlin_chat/server/UserRoomMediatorInteface.kt
@@ -1,9 +1,10 @@
 package com.lucaslpmoura.kotlin_chat.com.lucaslpmoura.kotlin_chat.server
 
 interface UserRoomMediatorInteface {
-    val userRoomMap : MutableMap<KotlinChatUser, KotlinChatRoom?>
-    val roomUserMap : MutableMap<KotlinChatRoom, MutableSet<KotlinChatUser>>
-
+    public fun addRoom(room: KotlinChatRoom)
+    public fun removeRoom(room: KotlinChatRoom)
+    public fun addUser(user: KotlinChatUser)
+    public fun removeUser(user: KotlinChatUser)
     public fun addUserToRoom(user: KotlinChatUser, room: KotlinChatRoom)
     public fun removeUserFromRoom(user: KotlinChatUser, room: KotlinChatRoom)
 }
