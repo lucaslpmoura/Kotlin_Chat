ccd6168db4304cf7391eb13372602d2613e87a10
Lucas <lucaslpmoura@gmail.com>
2026-04-30 09:35:18 -0300

    Changed KotlinChatRoom.kt to inherit KotlinChatComponent

--- src/main/kotlin/com/lucaslpmoura/kotlin_chat/server/KotlinChatRoom.kt
+++ src/main/kotlin/com/lucaslpmoura/kotlin_chat/server/KotlinChatRoom.kt
@@ -1,11 +1,6 @@
 package com.lucaslpmoura.kotlin_chat.com.lucaslpmoura.kotlin_chat.server
 
-class KotlinChatRoom(
-    val name: String,
-    val id: String,
-    private val mediator: UserRoomMediatorInteface
-) {
-
-    val MAX_USERS = 3
-    var numOfUsers = 0
+class KotlinChatRoom(id: String, name: String, mediator: UserRoomMediatorInteface, val MAX_USERS : Int)
+    : KotlinChatComponent(id, name, mediator ){
+    val numOfUsers : Int = 0
 }
