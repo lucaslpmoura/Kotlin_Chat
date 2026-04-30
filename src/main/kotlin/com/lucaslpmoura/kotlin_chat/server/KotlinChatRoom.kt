f2705784d06f843d54e6812e3557f15634a53766
Lucas <lucaslpmoura@gmail.com>
2026-04-30 10:01:34 -0300

    Changed numOfUsers from val to var

--- src/main/kotlin/com/lucaslpmoura/kotlin_chat/server/KotlinChatRoom.kt
+++ src/main/kotlin/com/lucaslpmoura/kotlin_chat/server/KotlinChatRoom.kt
@@ -2,5 +2,5 @@ package com.lucaslpmoura.kotlin_chat.com.lucaslpmoura.kotlin_chat.server
 
 class KotlinChatRoom(id: String, name: String, mediator: UserRoomMediatorInteface, val MAX_USERS : Int)
     : KotlinChatComponent(id, name, mediator ){
-    val numOfUsers : Int = 0
+    public var numOfUsers : Int = 0
 }
