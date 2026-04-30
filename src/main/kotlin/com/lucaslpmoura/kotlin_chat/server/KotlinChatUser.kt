b612c3dec850cffb17f89a02636a14eb46a9271a
Lucas <lucaslpmoura@gmail.com>
2026-04-30 14:47:18 -0300

    Changed KotlinChatUser to add address field

--- src/main/kotlin/com/lucaslpmoura/kotlin_chat/server/KotlinChatUser.kt
+++ src/main/kotlin/com/lucaslpmoura/kotlin_chat/server/KotlinChatUser.kt
@@ -1,6 +1,6 @@
 package com.lucaslpmoura.kotlin_chat.com.lucaslpmoura.kotlin_chat.server
 
-class KotlinChatUser(id: String, name: String, mediator: UserRoomMediatorInteface)
+class KotlinChatUser(id: String, val address: String, name: String, mediator: UserRoomMediatorInteface)
     : KotlinChatComponent(id, name, mediator)
 {
 }
