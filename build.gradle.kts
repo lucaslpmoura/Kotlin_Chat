104a6f4c9537e1f28c0b478a53da2057c3286f3a
Lucas <lucaslpmoura@gmail.com>
2026-05-04 10:00:53 -0300

    Changing Gradle configs to conform to Jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 #!/usr/bin/env kotlin
+import org.gradle.api.publish.maven.MavenPublication
 
 plugins {
     kotlin("jvm") version "2.2.0"
     id("java-library")
 }
+
+
 
 repositories {
     mavenCentral()
@@ -14,6 +17,8 @@ dependencies {
     testImplementation("io.kotest:kotest-assertions-core:6.1.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
 }
+
+
 
 tasks.test {
     useJUnitPlatform()
